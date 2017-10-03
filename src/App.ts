82745bb18d59e302714c09ff4791a4aa696b665e
0609hyeri <hyeri_0609@protonmail.com>
2017-10-03 10:09:47 +0000

    init

---
+++ src/App.ts
@@ -0,0 +1,29 @@
+class SimpleGame {
+
+    constructor() {
+        this.game = new Phaser.Game(800, 800, Phaser.AUTO, 'content', { preload: this.preload, create: this.create, render: this.render });
+    }
+
+    game: Phaser.Game;
+
+    preload() {
+        this.game.load.image('runman', 'assets/run.png');
+    }
+
+    create() {
+        this.game.stage.backgroundColor = "#4488AA";
+        var runman = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'runman');
+        runman.anchor.setTo(0.5, 0.5);
+    }
+    
+    render() {
+        this.game.debug.text(this.game.world.centerX.toString(), 100, 100 );
+    }
+
+}
+
+window.onload = () => {
+
+    var game = new SimpleGame();
+
+};
